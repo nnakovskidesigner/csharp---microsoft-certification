c776dc719b5d235a8d1c575bceb45c2baa9cfe87
Nikola Nakovski <117409706+nnakovskidesigner@users.noreply.github.com>
2023-11-03 15:37:20 +0100

    ..
    .

---
+++ CsharpProjects/TestProject/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Random dice = new Random();
+int roll = dice.Next(1, 7);
+Console.WriteLine(roll);
+
+
+int result = Random.Next();
